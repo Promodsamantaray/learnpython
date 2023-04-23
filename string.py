00b8fbbff9fd1129c1d8ae74fc18726bd407eeca
Promodsamantaray <pramodray9348@gmail.com>
2023-04-23 15:27:09 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,32 @@
+# a = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntut labore et dolore magna aliqua.'
+# print(a)
+
+# get the chararcter from a string
+a = "Hello, World!"
+# print(a[1])
+# # iterarte over or Print each chahracter of string
+# for x in a:
+#   print(x)
+# #Print the length of string
+# print(len(a))
+#
+# txt = "The best things in life are free!"
+# print("free" in txt)
+#
+# txt = "The best things in life are free!"
+# if "freee" not in txt:
+#   print("Yes, 'free' is present.")
+# else:
+#     print("Not Available")
+# a = 333
+# b = 200
+# if b > a:
+#     print("b is greater than a")
+#     print("b is greater than axnncnjsd")
+
+f = open("D:\\NPKey.txt", "r")
+g = open("D:\\myfile.css", "x")
+# print(f.read())
+# print(f.readline())
+# for x in f:
+#   print(x)
